214af056f9cac4fd304564b576cdbdd7bb7bd185
David Allan <profissional@david-allan.com.br>
2016-12-20 18:25:19 -0200

    Added Morse protocol interface

---
+++ Morse code/protocol/IMorseType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Morse_code.protocol
+{
+    public interface IMorseType
+    {
+        Dictionary<string, string> GetAlphabet();
+        Dictionary<string, string> GetNumeral();
+    }
+}
