75512eadf912dab2d36cef863554883f716c16dc
David Allan <profissional@david-allan.com.br>
2016-12-20 18:25:59 -0200

    Created American Morse dictionary

---
+++ Morse code/protocol/AmericanMorse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Morse_code.protocol
+{
+    public class AmericanMorse : IMorseType
+    {
+        public Dictionary<string, string> GetAlphabet()
+        {
+            return new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                {"A","· —"},
+                {"B","— · · ·"},
+                {"C","· ·  ·"},
+                {"D","— · ·"},
+                {"E","·"},
+                {"F","· — ·"},
+                {"G","— — ·"},
+                {"H","· · · ·"},
+                {"I","· ·"},
+                {"J","— · — ·"},
+                {"K","— · —"},
+                {"L","——"},
+                {"M","— —"},
+                {"N","— ·"},
+                {"O","· ·"},
+                {"P","· · · · ·"},
+                {"Q","· · —  ·"},
+                {"R","·  · ·"},
+                {"S","· · ·"},
+                {"T","—"},
+                {"U","· · —"},
+                {"V","· · · —"},
+                {"W","· — —"},
+                {"X","· — · ·"},
+                {"Y","· ·  · ·"},
+                {"Z","· · ·  ·"},
+            };
+        }
+
+       public Dictionary<string, string> GetNumeral()
+        {
+            return new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                {"1","· — — ·"},
+                {"2","· · — · ·"},
+                {"3","· · · — ·"},
+                {"4","· · · · —"},
+                {"5","— — —"},
+                {"6","· · · · · ·"},
+                {"7","— — · ·"},
+                {"8","— · · · ·"},
+                {"9","— · · —"},
+                {"0","————"},
+            };
+        }
+    }
+}
