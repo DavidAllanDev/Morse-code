18bfa271265c8877d21f74e2f9de3484cf42a628
David Allan <profissional@david-allan.com.br>
2016-12-22 16:04:41 -0200

    Added UnitTest to solution
    Very first test maded to convert to Morse

---
+++ UnitTestMorse/MorseTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MorseCode.protocol;
+using MorseCode.text;
+
+namespace UnitTestMorse
+{
+    [TestClass]
+    public class MorseTest
+    {
+        [TestMethod]
+        public void CanConvertToMorse()
+        {
+            //Arrenge
+            var morser = new AmericanMorse();
+            var conversor = new StringConversor(morser);
+            var message = "Hi! My name is David!";
+
+            //Execute
+            var morseMessage = conversor.ConvertToMorse(message);
+
+            //Assert
+            Assert.AreNotEqual(morseMessage, null);
+            Assert.AreNotEqual(message, morseMessage);
+        }
+    }
+}
