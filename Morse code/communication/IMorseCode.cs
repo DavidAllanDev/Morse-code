a0466c2d771f265c5f08f721824afaf1408d840b
David Allan <profissional@david-allan.com.br>
2016-12-26 09:52:31 -0200

    Created MorseCode class to be able to encode to Morse
    The idea is to create a way to return data from morse to normal text

---
+++ Morse code/communication/IMorseCode.cs
@@ -0,0 +1,11 @@
+﻿namespace MorseCode.communication
+{
+    public interface IMorseCode
+    {
+        string WordSeparetor { get; }
+        bool PreservePreserveWeirdChars { get; }
+
+        string Morse(string text);
+        string UnMorse(string morse);
+    }
+}
---
+++ Morse code/communication/MorseCode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MorseCode.protocol;
+using MorseCode.text;
+
+namespace MorseCode.communication
+{
+    public class MorseCode : StringConversor, IMorseCode
+    {
+        private bool _preservePreserveWeirdChars = false;
+        private string _wordSeparetor = "";
+
+        public MorseCode(IMorseType moreseType) : base(moreseType) { }
+
+        public MorseCode(IMorseType moreseType, bool PreserveChars, string Separetor = "|") :
+         this(moreseType)
+        {
+            _preservePreserveWeirdChars = PreserveChars;
+            _wordSeparetor = Separetor;
+        }
+
+        public bool PreservePreserveWeirdChars
+        {
+            get { return _preservePreserveWeirdChars; }
+        }
+
+        public string WordSeparetor
+        {
+            get { return _wordSeparetor; }
+        }
+
+        public string Morse(string text)
+        {
+            return ConvertToMorse(text);
+        }
+
+        public string UnMorse(string morse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
